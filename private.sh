eead9202acdda3ac1be261f15311fe32f1b623be
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2020-03-06 10:14:09 -0600

    updated private.sh: new eCryptfs mount option name

--- private.sh
+++ private.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-ECRYPTFS_KEY=<run ecryptfs-add-passphrase and place sig here>
-
 arg=$1
 
 if [ -z "$arg" ]; then
         read -sp "Passphrase: " passphrase
-        sudo mount -t ecryptfs -o rw,relatime,ecryptfs_fnek_sig=${ECRYPTFS_KEY},ecryptfs_sig=${ECRYPTFS_KEY},ecryptfs_cipher=aes,ecryptfs_key_bytes=32,ecryptfs_unlink_sigs,verbosity=0,key=passphrase:passphrase_passwd="$passphrase" $HOME/.e $HOME/.private
+        sig=$(printf "%s" "${passphrase}" | ecryptfs-add-passphrase - | sed 's/\(.*\)\[\(.*\)\]\(.*\)/\2/g')
+        # Old mount options
+        #sudo mount -t ecryptfs -o rw,relatime,ecryptfs_fnek_sig="${sig}",ecryptfs_sig="${sig}",ecryptfs_cipher=aes,sig_bytes=32,ecryptfs_unlink_sigs,verbosity=0,key=passphrase:passphrase_passwd="$passphrase" ~/.e ~/.private
+        sudo mount -t ecryptfs -o rw,relatime,ecryptfs_fnek_sig="${sig}",ecryptfs_sig="${sig}",ecryptfs_cipher=aes,ecryptfs_key_bytes=32,ecryptfs_unlink_sigs,verbosity=0,key=passphrase:passphrase_passwd="$passphrase" ~/.e ~/.private
 else
         if [ "$arg" == "-u" ]; then
                 sudo umount ~/.private
