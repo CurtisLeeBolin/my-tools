7fb19558b03cdfb434cfccd2c01a84303ea078e8
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2020-11-21 23:18:02 -0600

    cleaned tabs/spaces

--- link-my-tools.sh
+++ link-my-tools.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 for i in ~/Projects/my-tools/*.{py,sh}; do
-	j="${i##*/}"
+  j="${i##*/}"
   j="${j%.??}"
 
-	if [ -f ~/.local/bin/"${j}" ]; then
- 		rm ~/.local/bin/"${j}"
-	fi
+  if [ -f ~/.local/bin/"${j}" ]; then
+    rm ~/.local/bin/"${j}"
+  fi
 
   ln -s "${i}" ~/.local/bin/"${j}"
 done
