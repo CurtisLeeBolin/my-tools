eeeda134ad71454f5b44b01b4398aa51143c633c
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2020-11-21 22:45:03 -0600

    updates

--- ydupdate.sh
+++ ydupdate.sh
@@ -5,15 +5,15 @@ if ! which git; then
   sudo pacman -Sy git
 fi
 
-if [[ -z ~/Projects ]]; then
+if ! [ -d ~/Projects ]; then
   mkdir ~/Projects
 fi
 
 cd ~/Projects
 
-if [[ -z youtube-dl ]]; then
-  git clone https://github.com/ytdl-org/youtube-dl.git
-else
+if [ -d youtube-dl ]; then
   cd youtube-dl
   git pull
+else
+  git clone https://github.com/ytdl-org/youtube-dl.git
 fi
